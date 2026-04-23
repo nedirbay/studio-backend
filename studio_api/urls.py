7b1c6340c504ed4a915bdbfd2825094056161ce0
nedirbay <nedirbay0098@gmail.com>
2026-04-23 19:00:14 +0500

    chore: update admin panel route from /admin/ to /adminpage/

--- studio_api/urls.py
+++ studio_api/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminpage/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
